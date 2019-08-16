8eb34ccf182d7c834f8c73695b63a0ce2728077b
Tom Heyning <1083768@student.ccgs.wa.edu.au>
2019-08-16 15:53:59 +0800

    Add side button functionality

--- TmsFrontend/StudentResultsMain.xaml.cs
+++ TmsFrontend/StudentResultsMain.xaml.cs
@@ -20,20 +20,49 @@ namespace TmsFrontend
     /// </summary>
     public partial class StudentResultsMain : Page
     {
+        List<TestInfo> TestInfos = new List<TestInfo> { };
+
         public StudentResultsMain()
         {
             InitializeComponent();
             setNames();
+            setTests();
+        }
+
+        public void setTests()
+        {
+            TestInfos.Add(new TestInfo()
+            {
+                Score = "21/25",
+                Percentage = "92%",
+                Grade = "A",
+                Questions = "5",
+                Average = "67%",
+                TimeLimit = "50 Minutes",
+                SetBy = "Graham Nolan",
+                Topic = "C#",
+                Title = "C# Practical Programming Test"
+            });
+
+            TestInfos.Add(new TestInfo()
+            {
+                Score = "22/25",
+                Percentage = "88%",
+                Grade = "A",
+                Questions = "5",
+                Average = "70%",
+                TimeLimit = "50 Minutes",
+                SetBy = "Graham Nolan",
+                Topic = "WPF",
+                Title = "WPF Test"
+            });
         }
 
         public void setNames()
         {
             List<ResultItem> items = new List<ResultItem>();
-            items.Add(new ResultItem() { ResultName = "Test A" });
-            items.Add(new ResultItem() { ResultName = "Test B" });
-            items.Add(new ResultItem() { ResultName = "Test C" });
-            items.Add(new ResultItem() { ResultName = "Test D" });
-            items.Add(new ResultItem() { ResultName = "Test E" });
+            items.Add(new ResultItem() { ResultName = "C# Practical Programming Test" });
+            items.Add(new ResultItem() { ResultName = "WPF Test" });
 
             ResultsList.ItemsSource = items;
         }
@@ -41,13 +70,45 @@ namespace TmsFrontend
         private void ChangeTest(object sender, RoutedEventArgs e)
         {
             var buttonHolder = sender as Button;
-            MessageBox.Show(buttonHolder.Content as String);
+            
+            changeTextBlocks(buttonHolder.Content as string);
+        }
+
+        private void changeTextBlocks(string selectedTest)
+        {
+            foreach (var item in TestInfos)
+            {
+                if(item.Title == selectedTest)
+                {
+                    TitleBox.Text = item.Title;
+                    ScoreBox.Text = item.Score;
+                    PercentageBox.Text = item.Percentage;
+                    GradeBox.Text = item.Grade;
+                    AverageBox.Text = item.Average;
+                    QuestionsBox.Text = item.Questions;
+                    TimeLimitBox.Text = item.TimeLimit;
+                    SetByBox.Text = item.SetBy;
+                    TopicBox.Text = item.Topic;
+                }
+            }
         }
     }
-
 
     public class ResultItem
     {
         public string ResultName { get; set; }
+    }
+    
+    public class TestInfo
+    {
+        public string Title { get; set; }
+        public string Score { get; set; }
+        public string Percentage { get; set; }
+        public string Grade { get; set; }
+        public string Average { get; set; }
+        public string Questions { get; set; }
+        public string TimeLimit { get; set; }
+        public string SetBy { get; set; }
+        public string Topic { get; set; }
     }
 }
