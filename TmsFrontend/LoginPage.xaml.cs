7fabdedea16b60c91c6d386324d6cb7a7bcb4f66
Tom Heyning <1083768@student.ccgs.wa.edu.au>
2019-10-29 18:13:02 +0800

    basic Login Page functionality

--- TmsFrontend/LoginPage.xaml.cs
+++ TmsFrontend/LoginPage.xaml.cs
@@ -18,13 +18,65 @@ namespace TmsFrontend
     /// <summary>
     /// Interaction logic for Page1.xaml
     /// </summary>
+    /// 
+
     public partial class Page1 : Page
     {
-        public int thirty = 30;
+        public string loginUserName { get; set; }
+
+        List<StudentLogin> logins = new List<StudentLogin> { };
 
         public Page1()
         {
             InitializeComponent();
+            DataContext = this;
+
+            createDummyStudent();
         }
+
+        private void Login(object sender, RoutedEventArgs e)
+        {
+            foreach(var item in logins)
+            {
+                if (loginUserName == item.CorrectUsername)
+                {
+                    if (this.PasswordBox.Password == item.CorrectPassword)
+                    {
+                        MessageBox.Show("Login Successful");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Login Credentials Incorrect");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Login Credentials Incorrect");
+                }
+            }
+        }
+
+        private void EnterKeyPasswordBox(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Return)
+            {
+                Login(PasswordBox, e);
+            }
+        }
+
+        public void createDummyStudent()
+        {
+            logins.Add(new StudentLogin()
+            {
+                CorrectUsername = "Moox",
+                CorrectPassword = "Angoos"
+            });
+        }
+    }
+
+    public class StudentLogin
+    {
+        public string CorrectUsername { get; set; }
+        public string CorrectPassword { get; set; }
     }
 }
